4761236f1a3d3292d8faf10a52c4d1eb39b02fff
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2023-10-27 12:46:21 +1300

    Added in CV

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,96 +1,118 @@
-import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
+import self from '../img/self.png';
+import mock1 from '../img/mock1.png';
+import mock2 from '../img/mock2.png';
+import mock3 from '../img/mock3.png';
 
-
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+export let colors = ['rgb(0,255,164)', 'rgb(166,104,255)'];
 
 export const info = {
-    firstName: "Nick",
-    lastName: "Walton",
-    initials: "NW", 
-    position: "a Full Stack Developer",
-    selfPortrait: self, 
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
-    baseColor: colors[0],
-    miniBio: [
-        {
-            emoji: '🌎',
-            text: 'based in the Wellington, NZ'
-        },
-        {
-            emoji: "💼",
-            text: "Fullstack Engineer"
-        },
-        {
-            emoji: "📧",
-            text: "nick@waltonit.dev"
-        }
+  firstName: 'Nick',
+  lastName: 'Walton',
+  initials: 'NW',
+  position: 'a Full Stack Developer',
+  selfPortrait: self,
+  gradient: `-webkit-linear-gradient(135deg, ${colors})`,
+  baseColor: colors[0],
+  miniBio: [
+    {
+      emoji: '🌎',
+      text: 'based in the Wellington, NZ',
+    },
+    {
+      emoji: '💼',
+      text: 'Fullstack Engineer',
+    },
+    {
+      emoji: '📧',
+      text: 'nick@waltonit.dev',
+    },
+  ],
+  socials: [
+    {
+      link: 'https://github.com/Nick6464',
+      icon: 'fa fa-github',
+      label: 'github',
+    },
+    {
+      link: 'https://www.linkedin.com/in/nick-walton-1a474b173/',
+      icon: 'fa fa-linkedin',
+      label: 'linkedin',
+    },
+    {
+      link: 'https://www.waltonit.dev/cv.pdf',
+      icon: 'fa fa-file',
+      label: 'cv',
+    },
+  ],
+  bio: "Hello! I'm Nick. I'm a Fullstack Engineer at Liquid IT. I've been using code to improve my life and the lives of others since I was 11. I'm passionate about using code to solve problems and make life easier. I'm a big fan of React and Node.js, and I'm always looking to learn new things.",
+  skills: {
+    proficientWith: [
+      'javascript',
+      'react',
+      'git',
+      'github',
+      'html5',
+      'css3',
+      'sql',
+      'postgres',
+      'azure',
+      'python',
     ],
-    socials: [
-        {
-            link: "https://github.com/Nick6464",
-            icon: "fa fa-github",
-            label: 'github'
-        },
-        {
-            link: "https://www.linkedin.com/in/nick-walton-1a474b173/",
-            icon: "fa fa-linkedin",
-            label: 'linkedin'
-        }
-
+    exposedTo: [
+      'aws',
+      'c#',
+      'java',
+      'c++',
+      'c',
+      'php',
+      'docker',
+      'powershell',
+      'terraform',
     ],
-    bio: "Hello! I'm Nick. I'm a Fullstack Engineer at Liquid IT. I've been using code to improve my life and the lives of others since I was 11. I'm passionate about using code to solve problems and make life easier. I'm a big fan of React and Node.js, and I'm always looking to learn new things.",
-    skills:
-        {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'html5', 'css3', 'sql', 'postgres', 'azure', 'python'],
-            exposedTo: ['aws', 'c#', 'java', 'c++', 'c', 'php', 'docker', 'powershell', 'terraform']
-        }
-    ,
-    hobbies: [
-        {
-            label: 'plants and tissue culture',
-            emoji: '🪴'
-        },
-        {
-            label: 'ctf competitions',
-            emoji: '🛡️'
-        },
-        {
-            label: 'dungeons and dragons',
-            emoji: '🐉'
-        },
-        {
-            label: '3d printing',
-            emoji: '🖨️'
-        },
-        {
-            label: 'gaming',
-            emoji: '🎮'
-        },
-        {
-            label: 'surfing',
-            emoji: '🏄'
-        }
-    ],
-    portfolio: [ 
-        {
-            title: "GaragePi",
-            live: "https://www.garagepi.site", 
-            source: "https://github.com/Nick6464/garagepi-frontend", 
-            image: mock1
-        },
-        {
-            title: "Minecraft Hardcore Tracker",
-            source: "https://github.com/Nick6464/Nicks-Hardcore",
-            image: mock2
-        },
-        {
-            title: "Portfolio",
-            live: "https://www.waltonit.dev",
-            source: "https://github.com/Nick6464/Portfolio",
-            image: mock3
-        },
-    ]
-}
+  },
+  hobbies: [
+    {
+      label: 'plants and tissue culture',
+      emoji: '🪴',
+    },
+    {
+      label: 'ctf competitions',
+      emoji: '🛡️',
+    },
+    {
+      label: 'dungeons and dragons',
+      emoji: '🐉',
+    },
+    {
+      label: '3d printing',
+      emoji: '🖨️',
+    },
+    {
+      label: 'gaming',
+      emoji: '🎮',
+    },
+    {
+      label: 'surfing',
+      emoji: '🏄',
+    },
+  ],
+  portfolio: [
+    {
+      title: 'GaragePi',
+      live: 'https://www.garagepi.site',
+      source: 'https://github.com/Nick6464/garagepi-frontend',
+      image: mock1,
+    },
+    {
+      title: 'Minecraft Hardcore Tracker',
+      source: 'https://github.com/Nick6464/Nicks-Hardcore',
+      image: mock2,
+    },
+    {
+      title: 'Portfolio',
+      live: 'https://www.waltonit.dev',
+      source: 'https://github.com/Nick6464/Portfolio',
+      image: mock3,
+    },
+  ],
+};
